5ca5db92b73f5643d9e02339cb994feed0ae5c4f
Imran us Salam Mian <imran.mian@campusjaeger.de>
2024-04-16 20:07:50 +0200

    second commit

---
+++ src/views/AboutView.vue
@@ -0,0 +1,6 @@
+<template>
+  <div>
+    <h1>Welcome to the About Page</h1>
+    <router-link to="/">Go to Home Page</router-link>
+  </div>
+</template>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,6 @@
+<template>
+  <div>
+    <h1>Welcome to the Home Page</h1>
+    <router-link to="/about">Go to About Page</router-link>
+  </div>
+</template>
